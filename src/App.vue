c60cc0e19f8ee003cf6c61337eb4eec41a935a21
Simone <sdiricco.91@gmail.com>
2023-03-21 23:11:23 +0100

    finish ux

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
-  <v-app>
-    <v-main>
+  <v-app class="v-app-container">
+    <v-main class="v-main-container">
       <router-view></router-view>
     </v-main>
+    <v-footer app class="text-center d-flex flex-column v-footer-container">
+      <v-btn class="mx-4" icon="mdi-github" variant="text"></v-btn>
+
+      <v-divider></v-divider>
+
+      <div>{{ new Date().getFullYear() }} — <strong>Simone Di Ricco</strong></div>
+    </v-footer>
   </v-app>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
+const icons = ["mdi-github", "mdi-twitter", "mdi-linkedin", "mdi-instagram"];
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/components/RecordRtc2.vue
+++ src/components/RecordRtc2.vue
@@ -1,21 +1,25 @@
 <template>
   <div class="main-container">
-    <div class="controls">
-      <v-btn v-if="!isRecording" @click="startRecording" color="error" icon class="mr-1">
-        <v-icon icon="mdi-record-circle"></v-icon>
-      </v-btn>
-      <v-btn v-else @click="stopRecording" icon color="error">
-        <v-icon icon="mdi-stop-circle-outline"></v-icon>
-      </v-btn>
-    </div>
-    <video v-if="!isRecording && showVideoPlayer" ref="videoPlayer" controls autoplay></video>
-    <video v-else-if="isRecording" ref="previewPlayer" autoplay muted></video>
+    <template v-if="showVideoPlayer || isRecording">
+      <video v-if="!isRecording && showVideoPlayer" ref="videoPlayer" controls autoplay></video>
+      <video v-else-if="isRecording" ref="previewPlayer" autoplay muted></video>
+      <div class="controls mt-4">
+        <v-btn prepend-icon="mdi-stop-circle" size="x-large" v-if="isRecording" @click="stopRecording" variant="outlined" rounded="pill" color="error">
+          Interrompi
+        </v-btn>
+        <v-btn prepend-icon="mdi-download" class="mr-4" size="x-large" v-if="!isRecording" @click="downloadVideo" rounded="pill" color="blue"> Scarica </v-btn>
+        <v-btn prepend-icon="mdi-record-circle"  size="x-large" @click="startRecording" v-if="!isRecording" variant="outlined" rounded="pill" color="error"> Registra di nuovo </v-btn>
+      </div>
+    </template>
+    <template v-else>
+      <v-btn prepend-icon="mdi-record-circle" size="x-large" rounded="pill" class="mx-auto" variant="outlined" @click="startRecording" color="error"> Avvia adesso </v-btn>
+    </template>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, onUnmounted } from 'vue';
-import RecordRTC from 'recordrtc';
+import { ref, onMounted, onUnmounted, nextTick } from "vue";
+import RecordRTC from "recordrtc";
 
 const videoPlayer = ref<any>(null);
 const previewPlayer = ref<any>(null);
@@ -24,19 +28,20 @@ const showVideoPlayer = ref(false);
 let recorder: any = null;
 
 const startRecording = async () => {
-  isRecording.value = true;
 
   try {
     const stream = await navigator.mediaDevices.getDisplayMedia({ video: true });
-    
-    recorder = new RecordRTC(stream, { type: 'video' });
-    recorder.startRecording();
-    
+    isRecording.value = true;
 
-    // Mostra lo stream video mentre registri
-    previewPlayer.value.srcObject = stream;
+    nextTick(()=> {      
+      recorder = new RecordRTC(stream, { type: "video" });
+      recorder.startRecording();
+      previewPlayer.value.srcObject = stream;
+    })
+
+
   } catch (error) {
-    console.error('Error:', error);
+    console.error("Error:", error);
     isRecording.value = false;
   }
 };
@@ -55,9 +60,25 @@ const stopRecording = async () => {
   });
 };
 
+const downloadVideo = () => {
+  if (!videoPlayer.value.src) return;
+
+  const url = videoPlayer.value.src;
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = `screen-recording-${new Date().toISOString()}.webm`;
+  a.style.display = "none";
+  document.body.appendChild(a);
+  a.click();
+  setTimeout(() => {
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+  }, 100);
+};
+
 onMounted(() => {
-  if (!('mediaDevices' in navigator) || !('getDisplayMedia' in navigator.mediaDevices)) {
-    alert('La funzionalità di registrazione dello schermo non è supportata in questo browser.');
+  if (!("mediaDevices" in navigator) || !("getDisplayMedia" in navigator.mediaDevices)) {
+    alert("La funzionalità di registrazione dello schermo non è supportata in questo browser.");
   }
 });
 
@@ -70,15 +91,14 @@ onUnmounted(() => {
 </script>
 
 <style scoped>
-
-.controls{
+.controls {
   display: flex;
   align-items: center;
   justify-content: center;
   margin-bottom: 12px;
 }
 .main-container {
-  height: 100vh;
+  height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -86,9 +106,14 @@ onUnmounted(() => {
 }
 video {
   max-width: 100%;
-  height: 80vh;
-  border-radius: 4px;
-  border: 1px solid #333;
-  margin: 8px
+  max-height: 50vh;
+  margin: 8px;
+}
+
+.sizing {
+  min-height: 100px;
+  height: 100px;
+  min-width: 300px;
+  width: 300px;
 }
 </style>
